e360a8c758b6b320c2d02d92ded6de0e4f8466b5
peterQFR <petermilani80@gmail.com>
2020-05-08 20:54:42 +1000

    Made camera into class

--- capture_multiple.py
+++ capture_multiple.py
@@ -4,6 +4,41 @@ import time
 import cv2
 import numpy as np
 
+
+class CanonEOS65D():
+
+    def __init__(self, dir):
+
+        dir = os.path.abspath(args.dir)
+        self.context = gp.Context()
+        self.camera = gp.Camera()
+        self.camera.init(context)
+        self.path='{}/{}'.format(dir, time.strftime('%S_%M_%H_%d_%m_%Y',time.localtime()))
+        if not os.path.exists(path):
+            os.mkdir(path)
+        os.chdir(path)
+    
+    def capture(self, nImages):
+        cameradata=[]
+        files=[]
+        for i in range(args.nImages):
+            print('Capturing image')
+            file_path = camera.capture(gp.GP_CAPTURE_IMAGE, context)
+            camera.wait_for_event(gp.GP_EVENT_CAPTURE_COMPLETE, context)
+            print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+            target = os.path.join(os.getcwd(), file_path.name)
+            files.append(target)
+            camera_file = gp.check_result(camera.file_get(
+                file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL, context))
+        cameradata.append([camera_file, target])
+        for camera_file in cameradata:
+            print('Copying image to', camera_file[1])
+            camera_file[0].save(camera_file[1])
+    
+    def __del__(self):
+        camera.exit(context)
+        print("Context cleaaared")
+
 def parse_args():
     parser=argparse.ArgumentParser(description='Stack a series of images captured by a camera of the night sky')
     parser.add_argument('-d', '--dir', dest='dir', help='directory to the images from a run')
@@ -12,34 +47,6 @@ def parse_args():
 
 if __name__ == '__main__':
     
-    args = parse_args() 
-    dir = os.path.abspath(args.dir)
-    context = gp.Context()
-    camera = gp.Camera()
-    camera.init(context)
-    files=[]
-    path='{}/{}'.format(dir, time.strftime('%S_%M_%H_%d_%m_%Y',time.localtime()))
-    if not os.path.exists(path):
-            os.mkdir(path)
-    os.chdir(path)
-    cameradata=[]
-    for i in range(args.nImages):
-        print('Capturing image')
-        file_path = camera.capture(gp.GP_CAPTURE_IMAGE, context)
-        camera.wait_for_event(gp.GP_EVENT_CAPTURE_COMPLETE, context)
-        print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
-        target = os.path.join(os.getcwd(), file_path.name)
-        files.append(target)
-        camera_file = gp.check_result(camera.file_get(
-                file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL, context))
-        #file_data = gp.check_result(gp.gp_file_get_data_and_size(camera_file))
-        cameradata.append([camera_file, target])
-    for camera_file in cameradata:
-        print('Copying image to', camera_file[1])
-        camera_file[0].save(camera_file[1])
-
-        #img = cv2.imread(target)
-        #cv2.imshow('Captured_Image', img)
-        #cv2.waitKey(500)
-    camera.exit(context)
-    print("Context cleaaared")
+    args = parse_args()
+    cam = CanonEOS65D(args.dir)
+    cam.capture(args.nImages)
